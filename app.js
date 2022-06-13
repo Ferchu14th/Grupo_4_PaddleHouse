69b5bf4f3e0eec3b6c5280f82c8586fa4934e0c9
pablodagra <pablodagra@hotmail.com>
2022-06-13 20:50:25 -0300

    actualizacion de primeros archivos y carpetas

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-const express = require ('express');
-const path = require ('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-app.listen(3100,()=>console.log('http://localhost:'+3100));
-const pathPublic = path.resolve(__dirname, './public');
+app.listen(3100, () => console.log("http://localhost:" + 3100));
+const pathPublic = path.resolve(__dirname, "./public");
 app.use(express.static(pathPublic));
-app.get('/', (req,res)=>{res.sendFile(path.join(__dirname, '/views/home.html')
-)
-}
-
-)
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/index.html"));
+});
