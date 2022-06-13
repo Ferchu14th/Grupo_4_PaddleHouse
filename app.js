4332222b8d533a4e9e273291f5d2082f497fae83
MartBuf <marrtinbuf@gmail.com>
2022-06-13 20:28:40 -0300

    Estructura Inicial

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require ('express');
+const path = require ('path');
+const app = express();
+app.listen(3100,()=>console.log('http://localhost:'+3100));
+const pathPublic = path.resolve(__dirname, './public');
+app.use(express.static(pathPublic));
+app.get('/', (req,res)=>{res.sendFile(path.join(__dirname, '/views/home.html')
+)
+}
+
+)
