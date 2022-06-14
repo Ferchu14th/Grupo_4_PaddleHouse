8ec4a4c8dd39185e3366ea04db22aa26135935af
FedeSotoo <fedesoto28@gmail.com>
2022-06-14 20:47:58 -0300

    agrego rutas de cada vista y actualizo el app.js

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
 const app = express();
-app.listen(3050, () => console.log("http://localhost:" + 3050));
-const pathPublic = path.resolve(__dirname, "./public");
-app.use(express.static(pathPublic));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/index.html"));
+app.use(express.static('public'));
+
+
+app.listen(3050, ()=>{
+    console.log('http://localhost:3050/');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/productCart', (req,res)=>{
+    res.sendFile(__dirname + '/views/productCart.html');
+});
+
+app.get('/productDetail', (req,res)=>{
+    res.sendFile(__dirname + '/views/productDetail.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
 });
