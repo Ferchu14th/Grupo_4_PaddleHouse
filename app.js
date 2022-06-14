0e80d0bf713fa4de9193b7002fb0175ec112a2eb
MartBuf <marrtinbuf@gmail.com>
2022-06-14 18:01:03 -0300

    Estructura Inicial CSS home

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.listen(3100, () => console.log("http://localhost:" + 3100));
+app.listen(3050, () => console.log("http://localhost:" + 3050));
 const pathPublic = path.resolve(__dirname, "./public");
 app.use(express.static(pathPublic));
 app.get("/", (req, res) => {
