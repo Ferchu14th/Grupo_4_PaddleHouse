5ec46d2c33a5ae1e8444f68a0938eab788414e28
Ferchu <ferchu14th@gmail.com>
2022-07-02 12:03:39 -0300

    creando el patrón de diseño MVC manual para Paddle House

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
 const express = require('express');
 const app = express();
+
+const PORT = process.env.PORT || 3050;
+
+/*const mainRoutes = require('./routes/mainRoutes');*/
+
+
 app.use(express.static('public'));
 
 
-app.listen(3050, ()=>{
-    console.log('http://localhost:3050/');
-});
-
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/src/views/index.html');
 });
 
 app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
+    res.sendFile(__dirname + '/src/views/login.html');
 });
 
 app.get('/productCart', (req,res)=>{
-    res.sendFile(__dirname + '/views/productCart.html');
+    res.sendFile(__dirname + '/src/views/productCart.html');
 });
 
 app.get('/productDetail', (req,res)=>{
-    res.sendFile(__dirname + '/views/productDetail.html');
+    res.sendFile(__dirname + '/src/views/productDetail.html');
 });
 
 app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
+    res.sendFile(__dirname + '/src/views/register.html');
+});
+
+app.listen(PORT, () => {
+    console.log("listening on port " + PORT);
 });
---
+++ src/routes/mainRoutes.js
@@ -0,0 +1,6 @@
+const router = express.Router();
+const mainController = require('../controllers/mainController');
+
+router.get('/', mainController.index);
+
+module.exports = router;
