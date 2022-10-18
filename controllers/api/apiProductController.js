7ee1b704896e5e5f28fcc0952528ae3d368f6117
Ferchu14th <ferchu14th@gmail.com>
2022-10-18 00:18:56 -0300

    adiciones al ApiController de Producto

--- controllers/api/apiProductController.js
+++ controllers/api/apiProductController.js
@@ -1,25 +1,37 @@
-const db = require('../../database/models');
+const db = require("../../database/models");
 const sequelize = db.sequelize;
 
 const productsApiController = {
     list: (req, res) => {
         db.Products.findAll()
-            .then(products => {
-                let paletas = products.filter(product => product.category == "PALETA")
-                let accesorios = products.filter(product => product.category == "ACCESORIOS")
-                let bolsos = products.filter(product => product.category == "BOLSOS-PALETERO")
+            .then((products) => {
+                let paletas = products.filter(
+                    (product) => product.category == "PALETA"
+                );
+                let accesorios = products.filter(
+                    (product) => product.category == "ACCESORIOS"
+                );
+                let bolsos = products.filter(
+                    (product) => product.category == "BOLSOS-PALETERO"
+                );
+                let categorias = ["PALETA", "ACCESORIOS", "BOLSOS-PALETERO"];
                 let respuesta = {
                     count: {
                         status: 200,
                         total: products.length,
+                        totalPaletas: paletas.length,
+                        totalBolsos: bolsos.length,
+                        totalAccesorios: accesorios.length,
+                        totalCategorias: categorias.length,
+
                         url: "/api/products",
                         countByCategory: [
                             { paletas: paletas.length },
                             { accesorios: accesorios.length },
-                            { bolsos: bolsos.length }
-                        ]
+                            { bolsos: bolsos.length },
+                        ],
                     },
-                    data: products.map(product => {
+                    data: products.map((product) => {
                         return {
                             id: product.id,
                             category: product.category,
@@ -28,26 +40,26 @@ const productsApiController = {
                             image: "/images/uploads/" + product.image,
                             price: product.price,
                             model: product.model,
-                            detail: "/api/products/" + product.id
-                        }
-                    })
-                }
+                            detail: "/api/products/" + product.id,
+                        };
+                    }),
+                };
 
-                res.json(respuesta)
+                res.json(respuesta);
             })
 
             .catch(function (error) {
-                res.json({ status: 800 })
-            })
+                res.json({ status: 800 });
+            });
     },
 
     detail: (req, res) => {
         db.Products.findByPk(req.params.id)
-            .then(products => {
+            .then((products) => {
                 let respuesta = {
                     meta: {
                         status: 200,
-                        url: "/api/products/" + products.id
+                        url: "/api/products/" + products.id,
                     },
                     data: {
                         id: products.id,
@@ -57,19 +69,16 @@ const productsApiController = {
                         image: "/images/uploads/" + products.image,
                         price: products.price,
                         model: products.model,
-                    }
-                }
+                    },
+                };
 
-                res.json(respuesta)
+                res.json(respuesta);
             })
 
             .catch(function (error) {
-                res.json({ status: 800 })
-            })
-
+                res.json({ status: 800 });
+            });
     },
-
-
-}
+};
 
 module.exports = productsApiController;
